2cd560db6a2f27e0115fd0357807a37a6b49fe14
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-06-23 11:21:55 -0700

    update dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-store',
-    version='2.0b9',
+    version='2.0b10',
     url='https://github.com/blockchain-certificates/cert-store',
     license='MIT',
     author='Blockcerts',
