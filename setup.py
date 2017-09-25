b67af406ac3488fc8ecec81d6bcebdcfe7e6eb33
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-09-25 11:13:26 -0700

    latest cert-schema

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-store',
-    version='2.0.3',
+    version='2.0.4',
     url='https://github.com/blockchain-certificates/cert-store',
     license='MIT',
     author='Blockcerts',
